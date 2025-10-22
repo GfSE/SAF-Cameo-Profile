af77d36294541ca027caaf8d368e8db43a995242
haarer <haarer@github.com>
2025-10-22 20:32:26 +0200

    split saf and dev plugin into separate directories, generate plugin by script

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ If you need help, have a question or for general discussion, please visit [SAF d
 
 
 ## SAF Plugin (recommended for model users)
-**[SAF_Plugin.zip](SAF_Plugin/SAF_Plugin.zip)** installs the SAF Profile, additional diagram types for some viewpoints, additional project templates, and the FFDS Example Model. The plugin is required to open the SAF FFDS Example Model. The plugin also provides the SCM Profile, which can used to develop viewpoints in SAF.
+**[SAF_Plugin.zip](SAF_Plugin.zip)** installs the SAF Profile, additional diagram types for some viewpoints, additional project templates, and the FFDS Example Model. The plugin is required to open the SAF FFDS Example Model. 
 
 This plugin is for users of SAF wanting to create system models with SAF.
 
@@ -23,7 +23,7 @@ For development within the SAF Working group, only the Dev Plugin is necessary, 
 **[SAF_Profile.mdzip:](SAF_Plugin/profiles/SAF_Profile.mdzip)** The mdzip file contains the SAF Profile with SAF Stereotypes according to the [SAF-Specification](https://saf.gfse.org). See this [List of Stereotypes](https://saf.gfse.org/userdoc/stereotypes.html).
 
 ## SCM Profile
-**[SAF_SCM_Profile.mdzip:](SAF_Plugin/profiles/SAF_SCM_Profile.mdzip)** The mdzip file contains the SCM Profile used to develop SAF Viewpoints.
+**[SAF_SCM_Profile.mdzip:](SAF_DevPlugin/profiles/SAF_SCM_Profile.mdzip)** The mdzip file contains the SCM Profile used to develop SAF Viewpoints.
 This [development documentation](https://saf.gfse.org/devdoc/devdoc.html) intends to help with the modification or extension of SAF. If you need help with extending SAF, please contact the SAF developers at the [SAF discussion forum](https://github.com/GfSE/SAF-Specification/discussions).
 
 ## SAF FFDS Example Model
@@ -35,10 +35,9 @@ This [development documentation](https://saf.gfse.org/devdoc/devdoc.html) intend
 **[SAF_FFDS_NAF.mdzip:](SAF_Plugin/samples/SAF/SAF_FFDS_NAF.mdzip)** The mdzip file contains some NAF model elements as needed to demonstrate traceability in the SAF Specification. It is included in the SAF Plugin.
 
 ## SAF Dev Plugin
-**[SAF_DevPlugin.zip](SAF_Plugin/SAF_DevPlugin.zip)** installs only the Diagram Types. Its main use is for development of the SAF Profile, SAF Specification and so on. This is, because when developing SAF the user plugin should not be installed.
+**[SAF_DevPlugin.zip](SAF_DevPlugin.zip)** installs only the Diagram Types. Its main use is for development of the SAF Profile, SAF Specification and so on. This is, because when developing SAF the user plugin should not be installed.
 
 The SAF Dev Plugin is installed using the Resource/Plugin Manager which can be accessed via the help menu in Cameo.
-Do not install both plugins !
 
 ## Releases
 This is the TdSE2025 release. For the current state of development head over to the [main branch](https://github.com/GfSE/SAF-Cameo-Profile)
---
+++ genplugin.groovy
@@ -0,0 +1,146 @@
+#!/usr/bin/env groovy
+// GitHub Copilot
+
+import groovy.xml.MarkupBuilder
+import groovy.io.FileType
+import java.util.zip.ZipOutputStream
+import java.util.zip.ZipEntry
+import java.io.FileOutputStream
+
+// --- Constants / Header content ---
+def OUTPUT_FILENAME = 'SAF_Plugin/data/resourcemanager/MDR_Profile_SAF_75377_descriptor.xml'
+
+// resourceDescriptor attributes
+def RESOURCE_ATTRS = [
+	critical       : 'false',
+	date           : '25-10-22',
+	description    : "SAF Profile 1.2 TdSE2025,\nProvider Name: GfSE SAF Working Group",
+	group          : 'mac Resource',
+	homePage       : 'https://saf.gfse.org/version/TdSE2025',
+	id             : '75377',
+	mdVersionMax   : 'higher',
+	mdVersionMin   : '19.0',
+	name           : 'SAF Profile',
+	product        : 'SAF Profile',
+	restartMagicdraw: 'true',
+	type           : 'Profile'
+]
+
+// version and provider
+def VERSION = [human: '1.2', internal: '100011', resource: '1000110']
+def PROVIDER = [email: 'saf@gfse.org', homePage: 'https://saf.gfse.org/version/TdSE2025', name: 'GfSE SAF Working Group']
+
+// requiredResource
+def REQUIRED = [id: '1440', name: 'SysML', minVersion: [internal: '1900010', human: '19.0 SP3']]
+
+// editions list
+def EDITIONS = [
+	'Reader',
+	'Community',
+	'Standard',
+	'Professional Java',
+	'Professional C++',
+	'Professional C#',
+	'Professional',
+	'Architect',
+	'Enterprise'
+]
+
+// installation file list (each entry used for both from and to)
+// scan directories are inside the SAF_Plugin folder; strip that prefix for XML output
+def BASE_SCAN_ROOT = 'SAF_Plugin'
+def INSTALL_SCAN_DIRS = [
+    'data/defaults',
+    'profiles',
+    'samples',
+    'templates'
+]
+
+def INSTALLATION_FILES = []
+def baseDir = new File(BASE_SCAN_ROOT)
+def baseCanon = baseDir.exists() ? baseDir.canonicalPath.replace('\\','/') : new File('.').canonicalPath.replace('\\','/')
+
+INSTALL_SCAN_DIRS.each { dirPath ->
+    def dir = new File(baseDir, dirPath)
+    if (dir.exists() && dir.isDirectory()) {
+        dir.eachFileRecurse(FileType.FILES) { f ->
+            def full = f.canonicalPath.replace('\\','/')
+            // produce path relative to BASE_SCAN_ROOT so the XML entries do not include the prefix
+            def rel = full.replaceFirst("^${baseCanon}/?", '')
+            INSTALLATION_FILES << rel
+        }
+    }
+}
+
+// remove duplicates and sort for deterministic output
+INSTALLATION_FILES = INSTALLATION_FILES.unique().sort()
+
+// --- Determine output directory ---
+// If a directory path is provided as first arg, use it; otherwise use the current directory.
+def outDir = args && args[0] ? new File(args[0]) : new File('.')
+if (!outDir.exists()) outDir.mkdirs()
+def outFile = new File(outDir, OUTPUT_FILENAME)
+
+// --- Build XML ---
+outFile.withWriter('UTF-8') { writer ->
+    // exact XML declaration with standalone="yes"
+    writer.write('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n')
+    def xml = new MarkupBuilder(writer)
+    xml.doubleQuotes = true
+
+    xml.resourceDescriptor(RESOURCE_ATTRS) {
+        version(human: VERSION.human, internal: VERSION.internal, resource: VERSION.resource)
+        provider(email: PROVIDER.email, homePage: PROVIDER.homePage, name: PROVIDER.name)
+        EDITIONS.each { e -> mkp.yieldUnescaped("\n\t\t"); edition(e) } // create multiple <edition> elements
+        // required resource
+        requiredResource(id: REQUIRED.id, name: REQUIRED.name) {
+            minVersion(internal: REQUIRED.minVersion.internal, human: REQUIRED.minVersion.human)
+        }
+        // installation files
+        installation {
+            INSTALLATION_FILES.each { f ->
+                file(from: f, to: f)
+            }
+        }
+    }
+}
+
+println "Wrote ${outFile.absolutePath}"
+
+// create a zip of the SAF_Plugin tree (like the Makefile)
+// zip will be created in the chosen outDir as SAF_Plugin.zip
+def zipFile = new File(outDir, 'SAF_Plugin.zip')
+// the plugin directory is expected under outDir (since descriptor was written there)
+// fallback to local BASE_SCAN_ROOT if not present in outDir
+def pluginDir = new File(outDir, BASE_SCAN_ROOT)
+if (!pluginDir.exists()) pluginDir = new File(BASE_SCAN_ROOT)
+
+if (pluginDir.exists() && pluginDir.isDirectory()) {
+    zipFile.withOutputStream { fos ->
+        ZipOutputStream zos = new ZipOutputStream(fos)
+        try {
+            pluginDir.eachFileRecurse(FileType.FILES) { f ->
+                // path inside zip must be relative to the pluginDir and use forward slashes
+                def relPath = f.canonicalPath.replace('\\','/').replaceFirst("^" + pluginDir.canonicalPath.replace('\\','/') + "/?", '')
+                // skip adding the zip into itself if outDir == pluginDir parent
+                if (relPath == zipFile.name) return
+                ZipEntry entry = new ZipEntry(relPath)
+                zos.putNextEntry(entry)
+                f.withInputStream { is ->
+                    byte[] buffer = new byte[8192]
+                    int len
+                    while ((len = is.read(buffer)) > 0) {
+                        zos.write(buffer, 0, len)
+                    }
+                }
+                zos.closeEntry()
+            }
+        } finally {
+            zos.finish()
+            zos.close()
+        }
+    }
+    println "Created zip: ${zipFile.absolutePath}"
+} else {
+    println "No SAF_Plugin directory found to zip (looked at: ${pluginDir.absolutePath})"
+}
